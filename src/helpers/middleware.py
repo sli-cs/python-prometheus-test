59770e142fa56fce682e2a02f124c7c70d8eace2
Andrew N Golovkov <andrey.golovkov@lazada.com>
2017-09-08 14:19:35 +0300

    removed application name from labels and create special metric with application name and revision info

--- src/helpers/middleware.py
+++ src/helpers/middleware.py
@@ -1,27 +1,42 @@
 from flask import request
-from prometheus_client import Counter, Histogram
+from prometheus_client import Counter, Histogram, Gauge
 import time
 import sys
 
 REQUEST_COUNT = Counter(
     'request_count', 'App Request Count',
-    ['app_name', 'method', 'endpoint', 'http_status']
+    ['method', 'endpoint', 'http_status']
 )
 REQUEST_LATENCY = Histogram('request_latency_seconds', 'Request latency',
-    ['app_name', 'endpoint']
+    ['endpoint']
 )
 
+# app_build_info
+app_name = 'my cool app'
+revision = 'unknown'
+try:
+    with open('.git-revision') as f:
+        revision = f.read().rstrip()
+except IOError:
+    print('Cannot read file .git-revision')
+
+APP_VERSION = Gauge(
+    'app_build_info', 'App Version Information',
+    ['app', 'revision'],
+)
+
+APP_VERSION.labels(app_name, revision).set(1)
 
 def start_timer():
     request.start_time = time.time()
 
 def stop_timer(response):
     resp_time = time.time() - request.start_time
-    REQUEST_LATENCY.labels('webapp', request.path).observe(resp_time)
+    REQUEST_LATENCY.labels(request.path).observe(resp_time)
     return response
 
 def record_request_data(response):
-    REQUEST_COUNT.labels('webapp', request.method, request.path,
+    REQUEST_COUNT.labels(request.method, request.path,
             response.status_code).inc()
     return response
 
